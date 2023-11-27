8f9aab5aff1a1028d5aecc70a5838f449aaaca7b
Juan Carlos Rodríguez <jcrr.developer@gmail.com>
2023-11-27 20:47:58 +0100

    Update backdropoff.py

--- backdropoff.py
+++ backdropoff.py
@@ -1,6 +1,6 @@
-!/usr/bin/python
-# Author: 0xJuaNc4
-# Módulos
+#!/usr/bin/python
+#Author: 0xJuaNc4
+#Módulos
 import rembg  # instalar rembg: pip install rembg
 import numpy as np
 import os
@@ -8,7 +8,7 @@ import sys
 from PIL import Image
 from colorama import init, Fore
 
-# Paleta de colores
+#Paleta de colores
 GREEN = Fore.GREEN
 RED = Fore.RED
 YELLOW = Fore.YELLOW
@@ -16,7 +16,7 @@ CYAN = Fore.CYAN
 RESET = Fore.RESET
 
 def check_path(path):
-    # Comprueba que la ruta introducida sea correcta y contenga una imagen válida.
+    #Comprueba que la ruta introducida sea correcta y contenga una imagen válida.
     valid_extensions = [".png", ".webp"]
     if os.path.isfile(path) and any(path.lower().endswith(ext) for ext in valid_extensions):
         return True
@@ -30,36 +30,36 @@ def process_image(img_path):
         # Obtener el nombre de la imagen con extensión
         img_name = os.path.basename(img_path)
         
-        # Cargar imagen con PIL
+        #Cargar imagen con PIL
         inp_image = Image.open(img_path)
         print(f"\n{GREEN}[*]{RESET} Imagen encontrada\n")
         print(f"\n{GREEN}[*]{RESET} Imagen {YELLOW}{img_name}{RESET} cargada\n")
 
-        # Convertir la imagen en un array de numpy
+        #Convertir la imagen en un array de numpy
         inp_arr = np.array(inp_image)
 
-        # Aplicar la eliminación del fondo con rembg
+        #Aplicar la eliminación del fondo con rembg
         out_arr = rembg.remove(inp_arr)
 
-        # Creando imagen PIL a partir del array resultante
+        #Creando imagen PIL a partir del array resultante
         out_img = Image.fromarray(out_arr)
 
-        # Guardando la imagen resultante
+        #Guardando la imagen resultante
         out_img.save(f'backdropoff-{img_name}')
         print(f"\n{GREEN}[*]{RESET} Imagen sin fondo guardada en {YELLOW}{os.getcwd()}{RESET}\n")
     else:
         print(f"\n{RED}[!]{RESET} No se ha encontrado {img_path}, saliendo...\n")
 
-# Programa principal
+#Programa principal
 if __name__ == "__main__":
     while True:
-        # Solicitar al usuario que ingrese la ruta de la imagen
+        #Solicitar al usuario que ingrese la ruta de la imagen
         img_path = input(f"\n{CYAN}[*]{RESET} Ingresa la ruta de la imagen: ")
 
-        # Procesar la imagen
+        #Procesar la imagen
         process_image(img_path)
 
-        # Preguntar al usuario si desea realizar otra operación
+        #Preguntar al usuario si desea realizar otra operación
         decision = input(f"\n{CYAN}[*]{RESET} ¿Deseas procesar otra imagen? (s/n): ").lower()
         if decision != "s":
             print(f"\n{RED}[!]{RESET} Saliendo...\n")
