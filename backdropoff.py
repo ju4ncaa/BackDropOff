8f28f6daab0028a98a1cb7abbf80706cb9b91538
Juan Carlos Rodríguez <jcrr.developer@gmail.com>
2023-11-29 19:04:47 +0100

    Update backdropoff.py

--- backdropoff.py
+++ backdropoff.py
@@ -1,66 +1,74 @@
 #!/usr/bin/python
 #Author: 0xJuaNc4
+
 #Módulos
-import rembg  # instalar rembg: pip install rembg
-import numpy as np
 import os
-import sys
+from sys import exit
+from numpy import array
+from rembg import remove
 from PIL import Image
-from colorama import init, Fore
 
 #Paleta de colores
-GREEN = Fore.GREEN
-RED = Fore.RED
-YELLOW = Fore.YELLOW
-CYAN = Fore.CYAN
-RESET = Fore.RESET
+class Colors:
+    GREEN = "\033[92m"
+    RED = "\033[91m"
+    CYAN = "\033[96m"
+    YELLOW = "\033[93m"
+    RESET = "\033[0m"
 
+#Función limpiar consola
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
+
+#Comprueba que la ruta introducida sea correcta y contenga una imagen válida.
 def check_path(path):
-    #Comprueba que la ruta introducida sea correcta y contenga una imagen válida.
     valid_extensions = [".png", ".webp"]
     if os.path.isfile(path) and any(path.lower().endswith(ext) for ext in valid_extensions):
         return True
     else:
-        print(f"\n{RED}[!]{RESET} La ruta {YELLOW}{img_path}{RESET} no es válida o no tiene una extensión de imagen WebP o PNG.\n")
-        sys.exit(1)
+        print(f"\n{Colors.RED}[!]{Colors.RESET} La ruta {Colors.YELLOW}{img_path}{Colors.RESET} no es válida o no tiene una extensión de imagen WebP o PNG.")
+        exit(1)
 
+#Procesa la imagen, eliminando el fondo y guardando la nueva imagen.
 def process_image(img_path):
-    #Procesa la imagen, eliminando el fondo y guardando la nueva imagen.
     if check_path(img_path):
         # Obtener el nombre de la imagen con extensión
         img_name = os.path.basename(img_path)
         
         #Cargar imagen con PIL
         inp_image = Image.open(img_path)
-        print(f"\n{GREEN}[*]{RESET} Imagen encontrada\n")
-        print(f"\n{GREEN}[*]{RESET} Imagen {YELLOW}{img_name}{RESET} cargada\n")
+        print(f"\n{Colors.GREEN}[*]{Colors.RESET} Imagen encontrada\n")
+        print(f"\n{Colors.GREEN}[*]{Colors.RESET} Imagen {Colors.YELLOW}{img_name}{Colors.RESET} cargada\n")
 
         #Convertir la imagen en un array de numpy
-        inp_arr = np.array(inp_image)
+        inp_arr = array(inp_image)
 
         #Aplicar la eliminación del fondo con rembg
-        out_arr = rembg.remove(inp_arr)
+        out_arr = remove(inp_arr)
 
         #Creando imagen PIL a partir del array resultante
         out_img = Image.fromarray(out_arr)
 
         #Guardando la imagen resultante
         out_img.save(f'backdropoff-{img_name}')
-        print(f"\n{GREEN}[*]{RESET} Imagen sin fondo guardada en {YELLOW}{os.getcwd()}{RESET}\n")
+        print(f"\n{Colors.GREEN}[*]{Colors.RESET} Imagen sin fondo guardada en {Colors.YELLOW}{os.getcwd()}{Colors.RESET}\n")
     else:
-        print(f"\n{RED}[!]{RESET} No se ha encontrado {img_path}, saliendo...\n")
+        print(f"\n{Colors.RED}[!]{Colors.RESET} No se ha encontrado {img_path}")
 
 #Programa principal
 if __name__ == "__main__":
-    while True:
-        #Solicitar al usuario que ingrese la ruta de la imagen
-        img_path = input(f"\n{CYAN}[*]{RESET} Ingresa la ruta de la imagen: ")
-
-        #Procesar la imagen
-        process_image(img_path)
-
-        #Preguntar al usuario si desea realizar otra operación
-        decision = input(f"\n{CYAN}[*]{RESET} ¿Deseas procesar otra imagen? (s/n): ").lower()
-        if decision != "s":
-            print(f"\n{RED}[!]{RESET} Saliendo...\n")
-            break
+    try:
+        while True:
+            #Limpiar consola
+            clear_console()
+            #Solicitar al usuario que ingrese la ruta de la imagen
+            img_path = input(f"\n{Colors.CYAN}[*]{Colors.RESET} Ingresa la ruta de la imagen: ")
+            #Procesar la imagen
+            process_image(img_path)
+            #Preguntar al usuario si desea realizar otra operación
+            user_choice = input(f"\n{Colors.CYAN}[*]{Colors.RESET} ¿Deseas procesar otra imagen? (s/n): ").lower()
+            if user_choice != "s":
+                print(f"\n{Colors.RED}[!]{Colors.RESET} Saliendo...")
+                break
+    except KeyboardInterrupt:
+        print(f"\n\n{Colors.RED}[!]{Colors.RESET} Saliendo...")
